a4a25bfd4f1436e744a4c38475ae23a8a89345bb
gasaroleila <61538202+gasaroleila@users.noreply.github.com>
2022-04-06 16:03:03 +0200

    added appSidebar in plan layout

--- lib/layouts/main_plan_layout.dart
+++ lib/layouts/main_plan_layout.dart
@@ -4,6 +4,8 @@ import 'package:flutter_microsoft_todo_clone/widgets/widgets.dart';
 import 'package:flutter_microsoft_todo_clone/models/models.dart';
 import 'package:provider/provider.dart';
 
+import '../widgets/app_sidebar.dart';
+
 class MainPlanLayout extends StatelessWidget {
   final List<ToDo> data;
   final bool isPlanned;
@@ -20,80 +22,92 @@ class MainPlanLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double statusBar = MediaQuery.of(context).padding.top;
-    return Consumer<SideBar>(builder: (context,stateData, _) {
-      return Container(
-          padding: EdgeInsets.fromLTRB(24, statusBar + 12, 20, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: IconButton(
-                    icon: const Icon(Icons.menu, color: Colors.white, size: 32),
-                    onPressed: () => {stateData.changeSideBarDisplay()},
+    return Consumer<SideBar>(builder: (context, stateData, _) {
+      return Stack(children: [
+        Container(
+            padding: EdgeInsets.fromLTRB(24, statusBar + 12, 20, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: IconButton(
+                      icon:
+                          const Icon(Icons.menu, color: Colors.white, size: 32),
+                      onPressed: () => {stateData.changeSideBarDisplay()},
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                children: [
-                  AppHeader(
-                    title: isPlanned ? 'Planned' : 'Important',
-                    color: color,
-                    icon: headerIcon,
-                  ),
-                  isPlanned
-                      ? Container(
-                          padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                          alignment: Alignment.centerLeft,
-                          child: Container(
-                            width: 110,
-                            height: 40,
+                Column(
+                  children: [
+                    AppHeader(
+                      title: isPlanned ? 'Planned' : 'Important',
+                      color: color,
+                      icon: headerIcon,
+                    ),
+                    isPlanned
+                        ? Container(
+                            padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
                             alignment: Alignment.centerLeft,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.rectangle,
-                                borderRadius: BorderRadius.circular(5),
-                                color: Palette.secBlack),
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 3.0),
-                              child: Row(children: const [
-                                Padding(
-                                  padding:
-                                      EdgeInsets.symmetric(horizontal: 4.0),
-                                  child: Icon(
-                                    Icons.keyboard_arrow_down_rounded,
-                                    size: 25,
-                                    color: Colors.white,
+                            child: Container(
+                              width: 110,
+                              height: 40,
+                              alignment: Alignment.centerLeft,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.rectangle,
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Palette.secBlack),
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: 3.0),
+                                child: Row(children: const [
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 4.0),
+                                    child: Icon(
+                                      Icons.keyboard_arrow_down_rounded,
+                                      size: 25,
+                                      color: Colors.white,
+                                    ),
                                   ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(right: 8.0),
-                                  child: Text(
-                                    'Today',
+                                  Padding(
+                                    padding: EdgeInsets.only(right: 8.0),
+                                    child: Text(
+                                      'Today',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 17),
+                                    ),
+                                  ),
+                                  Text(
+                                    '2',
                                     style: TextStyle(
                                         color: Colors.white, fontSize: 17),
-                                  ),
-                                ),
-                                Text(
-                                  '2',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 17),
-                                )
-                              ]),
+                                  )
+                                ]),
+                              ),
                             ),
-                          ),
-                        )
-                      : const SizedBox.shrink(),
-                  TaskList(
-                    todos: data,
-                    isPlanned: isPlanned,
-                  ),
-                  const AddTask()
-                ],
-              ),
-            ],
-          ));
+                          )
+                        : const SizedBox.shrink(),
+                    TaskList(
+                      todos: data,
+                      isPlanned: isPlanned,
+                    ),
+                    const AddTask()
+                  ],
+                ),
+              ],
+            )),
+        stateData.show
+            ? Container(
+                width: MediaQuery.of(context).size.width * 0.85,
+                color: Colors.black,
+                child: ListView(
+                  children: const [AppSideBar()],
+                ),
+              )
+            : const SizedBox.shrink()
+      ]);
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyDayScreen(),
+        home: const TasksScreen(),
       ),
     );
   }
